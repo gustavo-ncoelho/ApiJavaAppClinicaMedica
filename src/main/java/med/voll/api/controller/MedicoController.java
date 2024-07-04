40faeb485c5ede5abd84b2a5df8a01e625e465a1
gustavo-ncoelho <gustavo-coelho@rede.ulbra.br>
2024-07-03 21:43:03 -0300

    commit geral

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,66 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.OverridesAttribute;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired   // <-- Injeção de dependência
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional  // <-- Essa notação é para dizer que haverá transação de dados com o banco de dados (Casos de put ou post)
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {  // <-- @Valid é necessário para verificar as validações feitas nos DTO'S
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri(); // <- Aqui será gerada a URI com o id do médico criado
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+    }
+
+    @GetMapping                        // Usa-se @PageableDefault para alterar o padrão de paginação
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {   // O retorno do tipo Page devolve a Lista mas também informações sobre a paginação
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new); // Aqui é convertido uma lista de Medicos para uma lista de DadosListagemMedico
+        //OBS: Para mudar a qtd de elementos por página pode-se mudar direto na url utilizando $size=1 // Para ordenação usa-se o filtro ?sort=nomeDoAtributo
+                                                          // O map() devolve uma Page
+        return ResponseEntity.ok(page);
+    }
+    @PutMapping
+    @Transactional
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+
+        return ResponseEntity.noContent().build(); // <-- Usa-se o build() para construir um objeto do tipo ResponseEntity
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico)); // <-- Usa-se o build() para construir um objeto do tipo ResponseEntity
+    }
+}
---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,26 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(  // <-- MedicoDTO
+        @NotBlank    //  <-- Aqui ficam as notações de validação dos dados
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefone,
+        @NotBlank @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid      //  <-- Aqui é necessário o @Valid pois precisa que o objeto endereco seja válido
+        DadosEndereco endereco
+){}
